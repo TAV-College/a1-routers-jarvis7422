0d7ec210c4d5daca98a064bb1a738b8a3991c6af
Aaditya Sahni <jarvisbot74@gmail.com>
2025-04-12 01:14:07 -0400

    all routes added

--- routers/booksRouter.js
+++ routers/booksRouter.js
@@ -10,12 +10,40 @@ const Book = require('../models/book'); // Adjust the path as necessary
 router.get('/', async (req, res) => {
   try {
     const books = await Book.getAllBooks();
-    res.render('show',  {books} );
+    res.render('showALLBooks',  {books} );
   } catch (error) {
     res.status(404).send('Book not found');
   }
 });
 
+router.get('/new', async (req,res)=>{
+  try{
+    res.render('newBook');
+  }catch(error){
+    res.status(404).send('Page not found');
+  }
+})
+
+router.get('/:id', async (req,res) => {
+  try{
+    ID = req.params.id;
+    const books = await Book.getBookById(ID);
+    res.render('bookById',{books});
+  }catch(error){
+    res.status(404).send('Book not found');
+  }
+})
+
+router.post("/", async (req, res) => {
+  const author = req.body.author;
+  const title = req.body.title;
+  const isbn = req.body.isbn;
+
+  const result = await Book.addBook({ author, title, isbn});
+  const ID = result.lastInsertRowid;
+
+  res.redirect(`/books/${ID}`);
+});
 
 
 module.exports = router;
